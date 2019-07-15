7e3295a76d44e0e93f84e7bd75205d5931765637
Santiago Rivera <riveras8@montclair.edu>
2019-07-15 10:56:21 -0400

    Created Pokemon.cs

---
+++ PokemonApp/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonApp
+{
+    class Pokemon
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+        public string type { get; set; }
+
+    }
+}
